f0130a952895cf35979a1110bf2a92be91df6aa8
PragyanKumar <81701254+PragyanKumar@users.noreply.github.com>
2021-08-21 20:52:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
-menu_list_array = ["Veg Margherita Pizza",
-"Chicken tikki pizza",
- "Paneer tikki pizza",
- "Classic pizza" ,
- "Supreme classic pizza",
- "Chicken fry pizza",
- "Supreme cheesy pizza",
- "Deluxe cheese chicken pizza",
-                    ];
+menu_list_array = ["Chicken Tandoori Pizza",
+                   "Veg Supreme Pizza",
+                   "Panner Tikka Pizza",
+                   "Deluxe Veggie Pizza",
+                   "Veg Extravaganza Pizza"]
 
 function getmenu(){
-var htmldata;
-htmldata="<ol class 'menulist'>"
-menu_list_array.sort();
-for(var i=0; i<menu_list_array.length;i++){
-    htmldata+'<li>' + menu_list_array[i]+ '</li>'
-}
-htmldata=htmldata+"</ol>"
-document.getElementById("display_menu").innerHTML=htmldata;
-//Complete the code
-}
-
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-menu_list_array.sort()
-htmldata="<section class ='cards'>"
-for(var i=0;i<menu_list_array;i++){
-    htmldata=htmldata+'<div class="card">'
-    +'<img src="images/pizzaImg.png"/>'
-    +menu_list_array[i]+'</div>'
-}
-htmldata=htmldata+"</section>"
-document.getElementById("display_addedmenu").innerHTML=htmldata;
-//Complete the code
-
-}
-
-function add_top(){
-    var item=document.getElementById("add_item").value;
-menu_list_array.push(item);
-add_item()
-//Complete the code
-}
+    
+     var htmldata;
+      htmldata="<ol class='menulist'>"
+       menu_list_array.sort();
+        for(var i=0; i<menu_list_array.length;i++){
+             htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>' 
+            }
+              htmldata=htmldata+"</ol>"
+               document.getElementById("menu1").innerHTML = htmldata; 
+        }
+        function addi(){ 
+             var htmldata;
+              var item=document.getElementById("add_item").value;
+               menu_list_array.push(item);
+                menu_list_array.sort();
+                 htmldata="<section class='cards'>"
+                  for(var i=0 ; i < menu_list_array.length; i++){ 
+           
+          htmldata=htmldata + '<div class="card">' + '<img src="pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+     }
+      htmldata=htmldata + "</section>"
+       document.getElementById("display_menu").innerHTML = htmldata; 
+                  }
